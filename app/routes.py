8fc070ea88f733db040462f5f957fa17eece97cc
Dev Suthar <46597688+d3vsuthar@users.noreply.github.com>
2022-07-25 17:21:11 +0400

    add sqlite3 db helpers

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,22 @@
 from app import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, g, request, jsonify, make_response
+import sqlite3
+
+DATABASE = '' # path to database file goes here
+
+# sqlite db helpers
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def home():
